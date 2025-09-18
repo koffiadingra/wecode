318734ab0d6c9c1114238f7f4d5b808ea158bc45
Adingra koffi jean emmanuel martial <koffi.adingra@epitech.eu>
2025-09-18 01:39:49 +0000

    Modif Test _registerV.vue

--- projet semaine ratrap/vuejs_prod/src/components/Test _registerV.vue
+++ projet semaine ratrap/vuejs_prod/src/components/Test _registerV.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { reactive } from 'vue';
-import { useAuthStore } from '@/stores/User';
+import { useAuthStore } from '@/stores/Users';
 import { useRouter } from 'vue-router';
 
 const authStore = useAuthStore();
