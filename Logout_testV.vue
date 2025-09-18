bf3a5bbd40ee481d855119c57394936955d4c4b0
Adingra koffi jean emmanuel martial <koffi.adingra@epitech.eu>
2025-09-18 01:38:50 +0000

    Create Logout_testV.vue

---
+++ Logout_testV.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useAuthStore } from '@/stores/User';
+import { useRouter } from 'vue-router';
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+const handleLogout = () => {
+  authStore.logout();
+  router.push('/login');
+};
+</script>
+
+<template>
+  <button @click="handleLogout">Déconnexion</button>
+</template>
