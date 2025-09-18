b5f020cc7685d9735ca3403a4fbe1265a35b5dbf
Adingra koffi jean emmanuel martial <koffi.adingra@epitech.eu>
2025-09-18 01:36:18 +0000

    Create Test_loginV.vue

---
+++ projet semaine ratrap/vuejs_prod/src/components/Test_loginV.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { reactive } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+import { useRouter } from 'vue-router';
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+const credentials = reactive({ email: '', password: '' });
+
+const onLogin = async () => {
+  try {
+    await authStore.login(credentials);
+    router.push('/contacts'); // Redirige vers l’interface protégée
+  } catch (err) {
+    alert(authStore.getError);
+  }
+};
+</script>
+
+<template>
+  <form @submit.prevent="onLogin">
+    <input v-model="credentials.email" type="email" placeholder="Email" required />
+    <input v-model="credentials.password" type="password" placeholder="Mot de passe" required />
+    <button type="submit" :disabled="authStore.isLoading">
+      {{ authStore.isLoading ? 'Connexion...' : 'Se connecter' }}
+    </button>
+  </form>
+</template>
