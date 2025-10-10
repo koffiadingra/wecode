2d62ed78b8a2f417414428f4ada354492d65da5b
Adingra koffi jean emmanuel martial <koffi.adingra@epitech.eu>
2025-10-10 08:56:54 +0000

    Update Testmanga.jsx

--- Testmanga.jsx
+++ Testmanga.jsx
@@ -43,7 +43,7 @@ function MangaSearch() {
         </button>
       </form>
 
-      {loading && <p>Chargement...</p>}
+      {loading && <p>loading...</p>}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {mangas.map((manga) => (
