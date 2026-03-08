173a775ba536fbbcb878fc47284676afe410c105
koffiadingra <koffi0.adingra@epitech.eu>
2026-03-08 21:24:34 +0000

    test11

--- gestion/xsel_gestion_backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ gestion/xsel_gestion_backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('otp', 255)->nullable();          
+            $table->timestamp('otp_expires_at')->nullable();   
+            $table->timestamp('otp_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
